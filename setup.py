c9d6bc5bea33b3bf00ccb8410cfb57ac0395470e
Eric Oswald <eoswald39@gmail.com>
2017-04-11 20:14:09 -0400

    Rst readme (#2)
    * Convert README to rST for pypi
    
    * Fix syntax highlighting
    
    * Fix one more thing and add pypi badge
    
    * Fix inline code
    
    * Increment version and point setup.cfg to new rst README.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='serialstruct',
-        version='1.0.0',
+        version='1.0.1',
         description=('Implements a StructuredPacket for pySerial\'s '
             'serial.threaded module'),
         author='Eric Oswald',
