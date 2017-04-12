44f8ea2ce016497933d32279575a679920c06146
Eric Oswald <eoswald39@gmail.com>
2017-04-12 00:07:42 -0400

    Increment version to 1.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='serialstruct',
-        version='1.0.1',
+        version='1.0.2',
         description=('Implements a StructuredPacket for pySerial\'s '
             'serial.threaded module'),
         author='Eric Oswald',
